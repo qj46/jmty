96d436fe634aff3a099d8d6d1338607d78a241ef
Your Name <you@example.com>
2023-06-13 23:20:51 +0000

    IPアドレスでアクセス制限

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdminController < ApplicationController
+  before_filter :block_foreign_hosts
+  
   def index
     @posts = Post.all
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,16 @@ class ApplicationController < ActionController::Base
   def set_should_render_navbar
     @should_render_navbar = false
   end
+  
+  private
+  # アクセスを許可するIPアドレス
+  def whitelisted?(ip)
+    return true if ENV['IP'] === ip
+    false
+  end
+
+  #　whiltelisted?で許可したIPアドレス以外のアクセスがあれば制限
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+  end
 end
