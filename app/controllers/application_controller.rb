5824161236499b564aca6ba6f8466b0f19306ab3
Your Name <you@example.com>
2023-06-13 23:27:23 +0000

    IPアドレスでアクセス制限な

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,16 @@ class ApplicationController < ActionController::Base
     @should_render_navbar = false
   end
   
-  # private
-  # # アクセスを許可するIPアドレス
-  # def whitelisted?(ip)
-  #   return true if ENV['IP'] === ip
-  #   false
-  # end
+  private
+  # アクセスを許可するIPアドレス
+  def whitelisted?(ip)
+    return true if ENV['IP'] === ip
+    false
+  end
 
-  # #　whiltelisted?で許可したIPアドレス以外のアクセスがあれば制限
-  # def block_foreign_hosts
-  #   return false if whitelisted?(request.remote_ip)
-  # end
+  #　whiltelisted?で許可したIPアドレス以外のアクセスがあれば制限
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    true
+  end
 end
