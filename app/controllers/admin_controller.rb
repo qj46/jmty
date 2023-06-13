c659751d4bfeb2dc9d94dd1d1cedb96738e222bb
Your Name <you@example.com>
2023-06-13 23:30:49 +0000

    IPアドレスでアクセス制限

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,4 +6,17 @@ class AdminController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  private
+  # アクセスを許可するIPアドレス
+  def whitelisted?(ip)
+    return true if ENV['IP'] === ip
+    false
+  end
+
+  #　whiltelisted?で許可したIPアドレス以外のアクセスがあれば制限
+  def block_foreign_hosts
+    return false if whitelisted?(request.remote_ip)
+    true
+  end
 end
