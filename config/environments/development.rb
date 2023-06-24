5e0272ee278ed8beaac82ef7d79dc01f86567082
Your Name <you@example.com>
2023-06-24 14:42:42 +0000

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,16 +42,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'myband.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name      => 'apikey',
-  :password       => ENV['SENDGRID_API_KEY'],
-  :domain => 'herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+    address: 'smtp.mail.yahoo.co.jp',
+    port: 465,
+    domain: 'smtp.mail.yahoo.co.jp',
+    user_name: ENV['MAIL_ADDRESS'],
+    password: ENV['PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger.
